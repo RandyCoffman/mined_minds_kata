702b006190c640fa9265ef53c5b1e0a843d2acc6
RandyCoffman <randy.coffman@wvjc.edu>
2018-02-08 14:33:48 -0500

    Replicated the code previously, but I used a counter instead of a while loop

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -1,9 +1,19 @@
-z = 0
-while z <= 99 
-	z += 1
-	if z == 5
+# z = 0
+# while z <= 99 
+# 	z += 1
+# 	if z == 5
+# 		puts "MinedMinds"
+# 	else
+# 		puts z
+# 	end
+# end
+
+counter = 1
+100.times do |counter|
+	counter = counter+1
+	if counter == 5
 		puts "MinedMinds"
-	else
-		puts z
+	else 
+		puts counter
 	end
 end
