a03c72e81d77c2210e839c7262562e4c54f16cd8
RandyCoffman <randy.coffman@wvjc.edu>
2018-02-08 15:13:56 -0500

    Current code only prints out even numbers from 1-100

--- print_to_screen.rb
+++ print_to_screen.rb
@@ -8,12 +8,30 @@
 # 	end
 # end
 
-counter = 1
-100.times do |counter|
-	counter = counter+1
-	if counter == 5
-		puts "MinedMinds"
-	else 
-		puts counter
+# counter = 1
+# 100.times do |counter|
+# 	counter = counter+1
+# 	if counter == 5
+# 		puts "MinedMinds"
+# 	else 
+# 		puts counter
+# 	end
+# end
+
+
+# counter = 100
+
+# while counter <= do
+# 	print counter
+# 	counter -= 1	
+# end
+
+
+
+z = 0
+100.times do |z|
+	z += 1
+	if z % 2 == 0
+		puts z
 	end
 end
