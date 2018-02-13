f016e29a3d08ae2a55bf177815848bbf812a0c74
RandyCoffman <randy.coffman@wvjc.edu>
2018-02-13 09:51:32 -0500

    I changed the if statements around to get the script tp run better

--- mined_minds_kata.rb
+++ mined_minds_kata.rb
@@ -1,15 +1,16 @@
-z = 0
-while z <= 99 
-	z += 1
-	if z == 3
+ z = 0
+
+ while z <= 99 
+ 	z += 1
+ 	if z == 3
 		puts "Mined"
-	elsif z % 3 == 0
-		puts "MinedMinds"
-	elsif z == 5
+ 	elsif z % 3 == 0 
+		puts "Mined Minds"
+ 	elsif z == 5
 		puts "Minds"
-	elsif z % 5 == 0
-		puts "MinedMinds"
-	else
-		puts z
-	end
+ 	elsif z % 5 == 0
+		puts "Mined Minds"
+ 	else
+ 		puts z
+ 	end
 end
