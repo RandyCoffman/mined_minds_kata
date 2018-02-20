528254e591abc964e0535f8bc43404a9c739943a
RandyCoffman <randy.coffman@wvjc.edu>
2018-02-20 15:30:52 -0500

    I added another number to test and named my tests to better understand them

--- mined_minds_kata_test.rb
+++ mined_minds_kata_test.rb
@@ -2,11 +2,16 @@ require "minitest/autorun"
 require_relative "mined_minds_kata_functions.rb"
 
 class Kata_test < Minitest::Test
-	def test_1_equals_1		#This is made to pass so I can establish a base
-		assert_equal(1,1)
+	def test_true_equals_true		#This is made to pass so I can establish a base
+		assert_equal(true,true)
 	end
 
-	def test_1_equals1
+	def test_1_returns_1
 		assert_equal(1, minedminds(1))
 	end
+
+	def test_7_returns_7
+		assert_equal(7, minedminds(7))
+	end
+
 end
