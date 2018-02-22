32ea906025afd0d7af005d8e9cf2c1da5fd1ce9e
RandyCoffman <randy.coffman@wvjc.edu>
2018-02-22 14:55:26 -0500

    I added a function saying if a number has no remainders divided by both 3 and 5 then replace those numbers with minedminds

--- mined_minds_kata_array_functions.rb
+++ mined_minds_kata_array_functions.rb
@@ -1,7 +1,9 @@
 def number_array
 	number = Array (1..100)
 	number.map!{|x|
-		if x % 3 == 0
+		if x % 3 == 0 && x % 5 == 0
+			x = "minedminds"
+		elsif x % 3 == 0
 			x = "mined"
 		elsif x % 5 == 0
 			x = "minds"
