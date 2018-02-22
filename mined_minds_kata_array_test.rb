c58e0515c3c3193f3d2cff2daaf9cfbcd03b5e76
RandyCoffman <randy.coffman@wvjc.edu>
2018-02-22 11:24:50 -0500

    first commit

---
+++ mined_minds_kata_array_test.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require_relative "mined_minds_kata_array_functions.rb"
+
+class Kata_array_test < Minitest::Test
+	def test_true_equals_true		#This is made to pass so I can establish a base
+		assert_equal(true,true)
+	end
+
+	def test_array_made?
+		assert_equal(Array, number_array.class)
+	end
+	
+end
