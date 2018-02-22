5dc181c56ec0ede7c90ad892db842a2c5b49efa1
RandyCoffman <randy.coffman@wvjc.edu>
2018-02-22 11:51:17 -0500

    fixed my 3rd test since it was hard coded and included further testing that passes

--- mined_minds_kata_array_test.rb
+++ mined_minds_kata_array_test.rb
@@ -11,7 +11,19 @@ class Kata_array_test < Minitest::Test
 	end
 
 	def test_array_range1_100
-		assert_equal([1..100], number_array)
+		assert_equal(100, number_array.length)
 	end
-	
+
+	def test_array_include49
+		assert_equal(true, number_array.include?(49))
+	end
+
+	def test_array_include100
+		assert_equal(true, number_array.include?(100))
+	end
+
+	def test_array_include14
+		assert_equal(true, number_array.include?(14))
+	end
+
 end
