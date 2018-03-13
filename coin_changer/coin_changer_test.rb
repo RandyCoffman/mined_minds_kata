2c081d63c3115628096de37e74564ee4116a4e36
RandyCoffman <randy.coffman@wvjc.edu>
2018-03-13 15:15:34 -0400

    first commit

---
+++ coin_changer/coin_changer_test.rb
@@ -0,0 +1,13 @@
+require "minitest/autorun"
+require_relative "coin_changer.rb"
+
+class Coin_changer < Minitest::Test
+	def test_true_equals_true		#This is made to pass so I can establish a base
+		assert_equal(true,true)
+	end
+
+	def test_class_hash
+		coin = coin_changer
+		assert_equal(Hash, coin)
+	end
+end
