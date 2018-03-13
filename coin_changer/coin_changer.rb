29e7b6a667731cee07c2ba682dc502950e81a81d
RandyCoffman <randy.coffman@wvjc.edu>
2018-03-13 16:32:54 -0400

    working function with the current test

--- coin_changer/coin_changer.rb
+++ coin_changer/coin_changer.rb
@@ -1,8 +1,18 @@
 def coin_changer(cents)
 	coins = {}
-	if cents / 5
-		coins ["nickel"] = cents 
-	elsif cents <= 4
+	if cents >= 25
+		coins ["quarter"] = cents / 25
+		cents = cents % 25
+	end
+	if cents >= 10
+		coins ["dime"] = cents / 10
+		cents = cents % 10
+	end
+	if cents >= 5
+		coins ["nickel"] = cents / 5
+		cents = cents % 5
+	end
+	if cents >= 1
 		coins["penny"] = cents
 	end
 	coins
